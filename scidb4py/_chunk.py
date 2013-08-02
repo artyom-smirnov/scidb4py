520ad7542068142848611fe38a7810aa75c9020f
Artyom Smirnov <smirnoffjr@gmail.com>
2013-08-02 15:21:19 +0400

    Fixed chunk decompression
    SciDB send uncompressed chunk data if size of uncompressed buffer equals size of compressed buffer (see array/MemArray.cpp)

--- scidb4py/_chunk.py
+++ scidb4py/_chunk.py
@@ -48,7 +48,7 @@ def make_chunk(chunk_msg, array):
     if sparse:
         raise NotImplementedError('Sparse chunks not supported yet')
 
-    if compression_method != 0:
+    if compression_method != 0 and len(chunk_data) != rec.decompressed_size:
         chunk_data = decompress(compression_method, chunk_data)
 
     start_pos = []
